3eb220064b124c86cbaf758bcaf42e2452ce0a32
Mister Abc <zl4ry@virginia.edu>
2014-04-11 23:00:26 -0400

    Added tess so it was not a subproject

---
+++ tess/eyes-two/obj/local/x86/objs/imageutils/blur.o.d
@@ -0,0 +1,108 @@
+obj/local/x86/objs/imageutils/blur.o: jni/imageutils/blur.cpp \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/math.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/sys/cdefs.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/android/api-level.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/sys/types.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/stdint.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/sys/_types.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/machine/_types.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/linux/posix_types.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/linux/stddef.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/linux/compiler.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/asm/posix_types.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/asm/posix_types_32.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/asm/types.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/linux/types.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/machine/kernel.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/sys/sysmacros.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/limits.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/sys/limits.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/linux/limits.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/machine/internal_types.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/machine/limits.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/sys/syslimits.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/asm/page.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/asm/page_32.h \
+ jni/imageutils/blur.h jni/imageutils/../common/types.h \
+ jni/imageutils/../common/utils.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/android/log.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/stdlib.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/string.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/malloc.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/alloca.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/strings.h \
+ /home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/memory.h \
+ jni/imageutils/../common/types.h
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/math.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/sys/cdefs.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/android/api-level.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/sys/types.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/stdint.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/sys/_types.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/machine/_types.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/linux/posix_types.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/linux/stddef.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/linux/compiler.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/asm/posix_types.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/asm/posix_types_32.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/asm/types.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/linux/types.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/machine/kernel.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/sys/sysmacros.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/limits.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/sys/limits.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/linux/limits.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/machine/internal_types.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/machine/limits.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/sys/syslimits.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/asm/page.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/asm/page_32.h:
+
+jni/imageutils/blur.h:
+
+jni/imageutils/../common/types.h:
+
+jni/imageutils/../common/utils.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/android/log.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/stdlib.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/string.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/malloc.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/alloca.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/strings.h:
+
+/home/zeming/adt/ndk/platforms/android-9/arch-x86/usr/include/memory.h:
+
+jni/imageutils/../common/types.h:
