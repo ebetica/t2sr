9df9711873e4d3eb1e5a27500d80a92e5692e5ef
Mister Abc <zl4ry@virginia.edu>
2014-04-12 02:34:48 -0400

    Added OCR code

---
+++ android/app/src/main/java/com/ttoosr/text2speed/app/ImageToText.java
@@ -0,0 +1,102 @@
+package com.ttoosr.text2speed.app;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Environment;
+import android.util.Log;
+
+import com.googlecode.leptonica.android.Pixa;
+import com.googlecode.tesseract.android.ResultIterator;
+import com.googlecode.tesseract.android.TessBaseAPI;
+import com.googlecode.tesseract.android.TessBaseAPI.PageIteratorLevel;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Created by zeming on 4/12/14.
+ */
+public class ImageToText {
+    private String TESSBASE_PATH;
+    private static final String DEFAULT_LANGUAGE = "eng";
+
+    private TessBaseAPI baseApi;
+
+    public ImageToText(Context context) {
+        copyAssets(context);
+        TESSBASE_PATH = context.getFilesDir().getPath();
+        Log.d("PATH_DEBUG", "Actual Path = " + TESSBASE_PATH);
+        baseApi = new TessBaseAPI();
+        baseApi.init(TESSBASE_PATH, DEFAULT_LANGUAGE);
+        baseApi.setPageSegMode(TessBaseAPI.PageSegMode.PSM_SINGLE_LINE);
+    }
+
+    private void copyAssets(Context context) {
+        AssetManager assetManager = context.getAssets();
+        String[] files = null;
+        try {
+            files = assetManager.list("");
+        } catch (IOException e) {
+            Log.e("PATH_DEBUG", "Failed to get asset file list.", e);
+        }
+        String dir = (new File(context.getFilesDir(), "tessdata")).getPath();
+        File tessdata = new File(dir);
+        if (!tessdata.mkdirs()) Log.d("PATH_DEBUG", "Failed to create tessdata dir");
+        for(String filename : files) {
+            InputStream in = null;
+            OutputStream out = null;
+            try {
+                in = assetManager.open(filename);
+                File outFile = new File(dir, filename);
+                Log.d("Copying", "Copying " + filename + " ... ");
+                out = new FileOutputStream(outFile);
+                Log.d("Copying", "...to " + outFile.getAbsolutePath());
+                copyFile(in, out);
+                Log.d("Copying", "Copied! Now we just need to close the streams");
+                in.close();
+                in = null;
+                out.flush();
+                out.close();
+                out = null;
+            } catch(IOException e) {
+                Log.e("PATH", "Failed to copy asset file: " + filename, e);
+            }
+        }
+    }
+    private void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while((read = in.read(buffer)) != -1){
+            out.write(buffer, 0, read);
+        }
+    }
+
+    public static Bitmap getTextImage(String text, int width, int height) {
+        final Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        final Paint paint = new Paint();
+        final Canvas canvas = new Canvas(bmp);
+
+        canvas.drawColor(Color.WHITE);
+
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setAntiAlias(true);
+        paint.setTextAlign(Paint.Align.CENTER);
+        paint.setTextSize(24.0f);
+        canvas.drawText(text, width / 2, height / 2, paint);
+
+        return bmp;
+    }
+
+    public String getString(Bitmap bmp) {
+        baseApi.setImage(bmp);
+        return baseApi.getUTF8Text();
+    }
+}
